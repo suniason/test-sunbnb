bbb8e977ef387085857b5f98d6298c24bc7a2569
suniason <galvezbrett17@gmail.com>
2024-02-26 16:35:18 +0800

    added cofig file

---
+++ frontend/src/app/providers.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+
+const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    return <NextUIProvider>{children}</NextUIProvider>;
+};
+
+export default Providers;
