aa19359ac0067675000df82f1c2db6b55cda964b
johnvergilaparriSA <johnvergil.aparri@sun-asterisk.com>
2024-02-27 15:43:44 +0800

    fixed errors for new component

--- frontend/src/app/components/HiComponent.tsx
+++ frontend/src/app/components/HiComponent.tsx
@@ -1,15 +1,15 @@
-import React, { ComponentProps } from 'react'
+import React from "react";
 
-interface HiProps{
-    greeting:string;
+interface HiProps {
+    greeting: string;
 }
 
-const HiComponent:React.FC<HiProps>= (props:{greeting:string}) => {
-return (
-<div className='bg-cyan-300 p-10 rounded-lg'>
-    <p className='text-2xl text-white font-bold'>{props.greeting}</p>
-</div>
-)
-}
+const HiComponent: React.FC<HiProps> = (props: { greeting: string }) => {
+    return (
+        <div className="rounded-lg bg-[#ff2200] p-10">
+            <p className="text-2xl font-bold text-white">{props.greeting}</p>
+        </div>
+    );
+};
 
-export default HiComponent
+export default HiComponent;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,12 @@
 import Image from "next/image";
 import React from "react";
 import Title from "./components/Title";
+import HiComponent from "./components/HiComponent";
 
 const Home: React.FC = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <HiComponent greeting="Here comes the Sun *Doo Doo Doo*"/>
             <Image src={"/logo.svg"} alt="Logo" width={500} height={500} />
             <Title title="List your space or find unique stays!" />
         </main>
