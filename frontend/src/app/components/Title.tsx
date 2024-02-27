aae7638bfb02943f93ce9e5f7a104d57c7ea8a77
Faith Rosalijos <faithabigail.rosalijos@sun-asterisk.com>
2024-02-27 14:28:29 +0800

    add title to home page

---
+++ frontend/src/app/components/Title.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface TitleProps {
+    title: string;
+}
+
+const Title: React.FC<TitleProps> = ({ title }) => {
+    return <h1 className="mt-4 text-6xl font-bold">{title}</h1>;
+};
+
+export default Title;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Image from "next/image";
 import React from "react";
+import Title from "./components/Title";
 
 const Home: React.FC = () => {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center p-24">
             <Image src={"/logo.svg"} alt="Logo" width={500} height={500} />
+            <Title title="List your space or find unique stays!" />
         </main>
     );
 };
