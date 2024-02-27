30018dbbf80a1be2a614aabdc7cbde18b7f8e3ed
Cara Encabo <caracarmel.encabo@sun-asterisk.com>
2024-02-27 15:15:56 +0800

    add button test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,12 +2,19 @@
 import Image from "next/image";
 import React from "react";
 import Title from "./components/Title";
+import Button from "./components/Button";
 
 const Home: React.FC = () => {
+
+    const handleClick = () => {
+        alert("Welcome!");
+    };
+
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
             <Image src={"/logo.svg"} alt="Logo" width={500} height={500} />
             <Title title="List your space or find unique stays!" />
+            <Button label="Get Started" onClick={handleClick} />
         </main>
     );
 };
