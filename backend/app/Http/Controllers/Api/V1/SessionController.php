e83bb2612187cbc50a8992c8c122cde354cdb76d
suniason <galvezbrett17@gmail.com>
2024-02-27 09:56:11 +0800

    some changes in the controller

--- backend/app/Http/Controllers/Api/V1/SessionController.php
+++ backend/app/Http/Controllers/Api/V1/SessionController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
-class SessionController extends Controller
-{
+class SessionController extends Controller {
     //
 }
--- backend/app/Http/Controllers/Api/V1/UserController.php
+++ backend/app/Http/Controllers/Api/V1/UserController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
-class UserController extends Controller
-{
+class UserController extends Controller {
     //
 }
